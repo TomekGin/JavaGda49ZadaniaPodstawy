93eda0f71e5b3ab6652d0626c441563dc89c8e4e
TomekGin <tomek.ginszt@gmail.com>
2021-09-11 15:59:41 +0200

    task10

---
+++ src/pl/sdacademy/java/basic/exercises/day1/Task10.java
@@ -0,0 +1,38 @@
+package pl.sdacademy.java.basic.exercises.day1;
+
+import java.util.Scanner;
+
+public class Task10 {
+    public static void main(String[] args) {
+        numbCalculator(125);
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please insert digit: ");
+        int number = scanner.nextInt();
+
+        System.out.println(sumOfDigits(number));
+    }
+
+    private static int sumOfDigits(int number) {
+        if (number <= 9) {
+            return number;
+        }
+
+        int sum = 0;
+        do {
+            sum += number % 10;
+            number /= 10;
+        } while (number != 0);
+        return 0;
+    }
+
+    private static void numbCalculator(int digit) {
+        int rest1 = digit % 10;
+        System.out.print(rest1);
+        int rest10 = ((digit - rest1) / 10) % 10;
+        System.out.print(" * " + rest10);
+        int rest100 = ((digit - rest10 - rest1) / 100) % 2;
+        System.out.println(" * " + rest100);
+    }
+
+}
